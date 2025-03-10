010e03c29b33e3737b1535e943cc5a3570314db2
Charles Sun <2209339564@qq.com>
2025-03-10 23:38:14 +0800

    fix typo

--- include/utils/glUtils.hpp
+++ include/utils/glUtils.hpp
@@ -5,23 +5,27 @@
 #include <chrono>
 
 #include <config.hpp>
-namespace scTracer::Utils {
-    class glUtils {
+namespace scTracer::Utils
+{
+    class glUtils
+    {
     public:
         static std::chrono::steady_clock::time_point formalTime;
-        static float getNow() {
+        static float getNow()
+        {
             auto now = std::chrono::high_resolution_clock::now();
             float res = std::chrono::duration<float, std::milli>(now - formalTime).count();
             formalTime = now;
             return res;
         }
-        static void checkError(const std::string& where) {
+        static void checkError(const std::string &where)
+        {
             GLenum err = glGetError();
-            if (err != GL_NO_ERROR) {
+            if (err != GL_NO_ERROR)
+            {
                 std::cerr << Config::LOG_RED << "OpenGL error in " << where << ": " << err << Config::LOG_RESET << std::endl;
             }
         }
     };
 
-    std::chrono::steady_clock::time_point glUtils::formalTime = std::chrono::high_resolution_clock::now();
 }
---
+++ src/utils/glUtils.cpp
@@ -0,0 +1,6 @@
+#include <utils/glUtils.hpp>
+
+namespace scTracer::Utils
+{
+    std::chrono::steady_clock::time_point glUtils::formalTime = std::chrono::high_resolution_clock::now();
+}
